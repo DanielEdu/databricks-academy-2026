2078007ebf6c329d65ddeb35b3bf42a3a0bfa09f
daniel.epr77@gmail.com <daniel.epr77@gmail.com>
2026-09-03 03:02:14 +0000

    nuevos cambios, bundles

--- main_local.py
+++ main_local.py
@@ -9,3 +9,17 @@ import os
 print("Python Runtime:", sys.version)
 print("Spark Version:", spark.version)
 print("Databricks Runtime Version:", os.environ.get("DATABRICKS_RUNTIME_VERSION"))
+
+# COMMAND ----------
+
+df = spark.createDataFrame(
+    [
+        (1, "Ana", "Data Engineer"),
+        (2, "Luis", "Data Scientist"),
+        (3, "Marta", "Analytics Engineer"),
+    ],
+    ["id", "nombre", "rol"],
+)
+
+
+df.show()
